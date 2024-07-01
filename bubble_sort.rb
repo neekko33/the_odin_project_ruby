c5f37f34b17d05246430fc20b417f990bdb05c0d
neekko33 <neekko33@outlook.com>
2024-07-01 21:23:48 +0800

    update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,33 @@
 nums = [4, 3, 78, 2, 0, 2]
 
+# def bubble_sort(nums)
+#   (0...nums.length - 1).each do |i|
+#     (0...nums.length - i - 1).each do |j|
+#       if nums[j] > nums[j + 1]
+#         temp = nums[j]
+#         nums[j] = nums[j + 1]
+#         nums[j + 1] = temp
+#       end
+#     end
+#   end
+#   nums
+# end
+
+# 更好的实现
 def bubble_sort(nums)
-  (0...nums.length - 1).each do |i|
-    (0...nums.length - i - 1).each do |j|
-      if nums[j] > nums[j + 1]
-        temp = nums[j]
-        nums[j] = nums[j + 1]
-        nums[j + 1] = temp
+  n = nums.length
+  loop do
+    swapped = false
+    (0...n - 1).each do |i|
+      if nums[i] > nums[i + 1]
+        nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        swapped = true
       end
     end
+    break unless swapped
   end
   nums
 end
 
-print bubble_sort(nums)
+p bubble_sort(nums)
 
